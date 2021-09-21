0a19124dd1602017f882ad77c46d137eebf7dd57
L-Steinmacher <dasai.sacul@gmail.com>
2021-09-20 18:39:16 -0700

    working on login

--- image-uploader-fe/src/components/CreateUser.js
+++ image-uploader-fe/src/components/CreateUser.js
@@ -14,6 +14,16 @@ export default function CreateUser() {
     const { push } = useHistory();
     const [formValues, setFormValues] = useState(initialFormValues);
 
+    const submitRegister = (value) => {
+        axios.post(`https://http://localhost:2019/user`, value)
+        .then(res => {
+            console.log(res)
+            push('/login')
+        }).catch(err => {
+            console.log(err);
+        })
+    }
+
     const onSubmit = (e) => {
         e.preventDefault();
         submitRegister(formValues);
@@ -28,8 +38,36 @@ export default function CreateUser() {
         <div>
             <h1>Create User</h1>
             <form onSubmit={onSubmit}>
+                <TextField
+                margin="normal"
+                label="Username"
+                placeholder="Username"
+                name="username"
+                onChange={onChange}
+                value={formValues.username}
+                variant="outlined"
+                />
 
+                <TextField
+                margin="normal"
+                label="Email"
+                variant="outlined"
+                placeholder="Email"
+                onChange={onChange}
+                value= {formValues.email}
+                name= "email"
+                />
 
+                <TextField
+                margin="normal"
+                type= "password"
+                onChange={onChange}
+                value= {formValues.password}
+                name= "password"
+                variant="outlined"
+                placeholder="Password"
+                label="Password"
+                />
                 <button type="submit" >Submit</button>
             </form>
         </div>
