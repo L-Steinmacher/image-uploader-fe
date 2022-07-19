b97e0548768322ebc0a5b4bb921a353348dc87f6
Lucas Steinmacher <lucaslsteinmacher@gmail.com>
2022-07-19 16:27:14 -0700

    feature: set up redux scafolding

--- image-uploader-fe/src/index.js
+++ image-uploader-fe/src/index.js
@@ -4,11 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { Provider } from 'react-redux';
+import {reducer} from './store'
 
 
+const store = createStore(reducer, applyMiddleware(thunk,logger));
 ReactDOM.render(
   <Router>
-    <App />
+    <Provider store={store} >
+      <App />
+    </Provider>
   </Router>,
   document.getElementById('root')
 );
